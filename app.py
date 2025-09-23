a0c4b03c3cd0b3591188685a084c95d89d9b2b8b
Egor Ulybin <egorulybin@gmail.com>
2025-09-23 23:33:04 +0200

    basic get and post request for api

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from stores import stores
 
 
 app = Flask(__name__)
+
+
+@app.get("/store")
+def get_stores():
+    return jsonify(stores)
+
+
+# @app.post("/store")
+# def add_store():
+#     data = request.get_json()
+#     new_store = {"store_name": data["store_name"], "items": data["items"]}
+#     stores.append(new_store)
+#     return jsonify(data), 201
+
+
+@app.post("/store/<string:store_name>")
+def add_store(store_name):
+    data = request.get_json()
+    for store in stores:
+        if store["store_name"] == store_name:
+            new_item = {"item_name": data["item_name"], "price": data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "No such store found."}, 404
--- stores.py
+++ stores.py
@@ -1,18 +1,4 @@
 stores = [
-    {
-        "name": "Mercadona",
-        "items": [
-            {"name": "gnochi", "prince": 2.51},
-            {"name": "milk", "prince": 1.16},
-            {"name": "tortilla", "prince": 3.22},
-        ],
-    },
-    {
-        "name": "Dia",
-        "items": [
-            {"name": "rice", "prince": 1.34},
-            {"name": "chicken", "prince": 6.56},
-            {"name": "salmon", "prince": 8.99},
-        ],
-    },
+    {"store_name": "Ikea", "items": [{"item_name": "lamp", "price": 17.99}]},
+    {"store_name": "Mercadona", "items": [{"item_name": "chicken", "price": 6.33}]},
 ]
